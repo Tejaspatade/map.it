cc44262e086b438d05d680992facef04752ad679
Tejas Patade <tej.patade69@gmail.com>
2023-06-27 11:54:33 +0530

    Using SearchParams

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,19 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
-import styles from "./City.module.css";
+// import styles from "./City.module.css";
 
 function City() {
 	const { id } = useParams();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const lat = searchParams.get("lat");
+	const lng = searchParams.get("lng");
 
 	// TEMP DATA
-	const currentCity = {
-		cityName: "Lisbon",
-		emoji: "🇵🇹",
-		date: "2027-10-31T15:59:59.138Z",
-		notes: "My favorite city so far!",
-	};
+	// const currentCity = {
+	// 	cityName: "Lisbon",
+	// 	emoji: "🇵🇹",
+	// 	date: "2027-10-31T15:59:59.138Z",
+	// 	notes: "My favorite city so far!",
+	// };
 
-	const { cityName, emoji, date, notes } = currentCity;
+	// const { cityName, emoji, date, notes } = currentCity;
 
 	return (
 		<h1>City Boiiii {id} </h1>

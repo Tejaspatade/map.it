f6b2f869053fe2d5375838febe09d90f8cfe4dae
Tejas Patade <tej.patade69@gmail.com>
2023-06-28 10:16:34 +0530

    Routing Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Product from "./pages/Product";
@@ -10,7 +10,9 @@ import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
+// Localhost for json-server to fetch data
 const BASE_URL = "http://localhost:8000";
 
 const App = () => {
@@ -18,7 +20,7 @@ const App = () => {
 	const [cities, setCities] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 
-	// Fetching City List as a sie-effect on mount
+	// Fetching City List as a side-effect on mount
 	useEffect(() => {
 		setIsLoading(true);
 		fetch(`${BASE_URL}/cities`)
@@ -36,12 +38,7 @@ const App = () => {
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route
-						index
-						element={
-							<CityList cities={cities} isLoading={isLoading} />
-						}
-					/>
+					<Route index element={<Navigate to="cities" replace />} />
 					<Route
 						path="cities"
 						element={
@@ -58,7 +55,7 @@ const App = () => {
 							/>
 						}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Map = () => {
+	// Routing Hooks
 	const [searchParams, setSearchParams] = useSearchParams();
+	const navigate = useNavigate();
+
+	//
 	const lat = searchParams.get("lat");
 	const lng = searchParams.get("lng");
+
 	return (
-		<div className={styles.mapContainer}>
+		<div
+			className={styles.mapContainer}
+			onClick={() => {
+				navigate("form");
+			}}
+		>
 			Map {lat} {lng}
 		</div>
 	);
